4e3d079ddc380cf10b6d9a8b65107b58ee957f60
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-09-06 10:01:02 +0200

    continuation js

---
+++ DecouverteJS/script3.js
@@ -0,0 +1,18 @@
+let monnaies = ["Euros", "Dollars américain", "Bitcoin"];
+monnaies["EUR"]="Euros";
+monnaies["USD"]="Dollars américains";
+monnaies["BTC"]="Bitcoin";
+console.table(monnaies);
+console.log(monnaies.length);
+
+let nom = "Cyril";
+console.log(nom);
+nom = "Maddie";
+console.log(nom);
+
+const age = 15;
+console.log(age);
+// age = 12;
+
+console.log(typeof(nom));
+console.log(typeof(age))
---
+++ DecouverteJS/tt2.js
@@ -0,0 +1,7 @@
+var data_ = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+data_[7] = 15;
+data_[8] = "Un autre jour";
+console.log(data_.lenght);
+document.write(data_);
+document.writeln(data_);
+alert(data_);
