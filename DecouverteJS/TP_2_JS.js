dad6d6cd8b01579ed4efbf6088d3b4a389003a03
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-09-07 10:04:26 +0200

    for dans un tableau

--- DecouverteJS/TP_2_JS.js
+++ DecouverteJS/TP_2_JS.js
@@ -5,8 +5,9 @@ document.write("Bonjour "+prenom);
 console.log("Bonjour " +prenom);
 
 //Exercice 1
-let a = 75;
-let b = 125;
+let a, b;
+a = Number(prompt("Entrez un nombre"));
+b = Number(prompt('entrez un autre nombre'));
 let result = a + b;
 alert("Le résultat est "+result);
 console.log("Le résultat est "+result);
@@ -21,11 +22,11 @@ console.log("la moyenne est "+moyenne);
 
 //Exeercice 3
 let budget = 100;
-let achats = 150;
-if (budget > achats){
+let achats = Number(prompt('Entrez le montant de vos achats'));
+if (budget >= achats){
     alert("Le budget permet de payer les achats");
     console.log("Le budget permet de payer les achats");
 }else{
-    alert("Le budget n'est pas suffisant pour payer les achats");
-    console.log("Le budget n'est pas suffisant pour payer les achats");
+    alert("Le budget n'est pas suffisant pour payer vos achats, votre budget restant est de 100 euros");
+    console.log("Le budget n'est pas suffisant pour payer vos achats, votre budget restant est de 100 euros");
 }
--- DecouverteJS/script3.js
+++ DecouverteJS/script3.js
@@ -16,3 +16,22 @@ console.log(age);
 
 console.log(typeof(nom));
 console.log(typeof(age))
+
+
+var mois = new Array(8);
+    mois[0]="Janvier";
+    mois[1]="Février";
+    mois[2]="Mars";
+    mois[3]="Avril";
+    mois[4]="Mai";
+    mois[5]="Juin";
+    mois[6]="Juillet";
+    mois[7]="Août";
+console.log(mois);
+
+    for(i=0; i<=mois.length; i++){
+        console.log("Parcourir le tableau avec une boucle " +mois[i])
+    }
+
+
+
