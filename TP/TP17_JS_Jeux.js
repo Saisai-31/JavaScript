2acb4b2c42381248b6c8c3eb329b4cb0594ccdc7
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-09-21 09:26:52 +0200

    finalisation du tp 17

--- TP/TP17_JS_Jeux.js
+++ TP/TP17_JS_Jeux.js
@@ -1,32 +1,39 @@
-if (confirm("Etes-vous prêt à commencer ?")){
 
-    var nombreAléatoire = Math.floor(Math.random() * 101);
-    console.log(nombreAléatoire);
-    var coups = 0;
-}
-function jeux(){
-    var val = document.getElementById("chiffre").value;
-    console.log(val);
-    while(true){
-        coups++;
-        
-        if(val){
-            if(val==nombreAléatoire){
-                document.getElementById("result").value = ("Bravo tu a trouvé le chiffre en " + coups + " essai(s) !!");
-                document.getElementById("result").style.color="red";
-            break;
-        }
-        else if(val < nombreAléatoire){
-            document.getElementById("result").value = ("C'est plus grand !");
-            document.getElementById("result").style.color="blue";
-            break;
-        }
-        else
-            document.getElementById("result").value = ("C'est plus petit !");
-            document.getElementById("result").style.color="green";
-        break;
-    }else{
-        break;
+var nombreAléatoire;
+    var coups;
+
+    function initialiserJeu() {
+      if (confirm("Etes-vous prêt à commencer ?")) {
+        nombreAléatoire = Math.floor(Math.random() * 101);
+        console.log(nombreAléatoire);
+        coups = 0;
+        document.getElementById("result").value = ""; // Réinitialiser le champ de résultat
+      }
     }
-}
-}
+
+    function jeux() {
+      var val = parseInt(document.getElementById("chiffre").value);
+
+      if (!isNaN(val)) {
+        coups++;
+
+        if (val === nombreAléatoire) {
+          document.getElementById("result").value = "Bravo tu as trouvé le chiffre en " + coups + " essai(s) !!";
+          document.getElementById("result").style.color = "red";
+        } else if (val < nombreAléatoire) {
+          document.getElementById("result").value = "C'est plus grand !";
+          document.getElementById("result").style.color = "blue";
+        } else {
+          document.getElementById("result").value = "C'est plus petit !";
+          document.getElementById("result").style.color = "green";
+        }
+      }
+    }
+
+    function rejouer() {
+      document.getElementById("chiffre").value = ""; // Réinitialiser le champ de saisie
+      initialiserJeu();
+    }
+
+    // Appeler initialiserJeu() pour commencer le jeu
+    initialiserJeu();
